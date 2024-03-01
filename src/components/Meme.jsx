271f020e236d51dedc9b677354e5e52072502da3
axiu <abawor200@gmail.com>
2024-03-01 20:52:20 +0000

    Added random memesdata file and random url generating

--- src/components/Meme.jsx
+++ src/components/Meme.jsx
@@ -1,6 +1,16 @@
 import React from "react"
+import memesData from "../memesData.js"
 
 export default function Meme() {
+    let url
+
+    function getNewMeme() {
+        const memesArray = memesData.data.memes
+        const randomNumber = Math.floor(Math.random() * memesArray.length)
+        url = memesArray[randomNumber].url
+        console.log(url)
+    }
+
     return (
         <main>
             <form className="form">
@@ -24,10 +34,12 @@ export default function Meme() {
                 </div>
                 <button 
                     className="form--button"
+                    onClick={getNewMeme}
                 >
                     Get a new meme image 🖼
                 </button>
             </form>
+            <img src={url} className="meme--image"></img>
         </main>
     )
 }
